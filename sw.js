74fa79bd1d3204fb1b0368b609c253d1cb6a5f32
chinguyen901 <128342123+chinguyen901@users.noreply.github.com>
2026-02-22 11:59:25 +0700

    Add service worker for caching files

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = "do-xanh-v1";
+const FILES = [
+  "./",
+  "./index.html",
+  "./icon.png",
+  "./manifest.json"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(c => c.addAll(FILES))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
